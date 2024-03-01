a17b8770e9182433e036ff1ae2e871741390de0c
Johnny Gérard <john@jgerard.dev>
2024-03-01 12:41:03 +0100

    feat: add TodoListService

---
+++ src/app/todo-list.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Signal, computed, signal } from '@angular/core';
+import { TodoItem } from './types/todo-item.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  #list = signal<TodoItem[]>([]);
+  activeList = computed(() => this.#list().filter(item => !item.completed()));
+  completedList = computed(() => this.#list().filter(item => item.completed()));
+  activeItemsCount = computed(() => this.#list().reduce(
+    (count, item) => count + (item.completed() ? 0 : 1), 0)
+  );
+
+  get list(): Signal<TodoItem[]> {
+    return this.#list.asReadonly();
+  }
+
+  addItem(description: string): void {
+    // Note: The todo item is added at the top of the list.
+    this.#list.update(list => [new TodoItem(description), ...list]);
+  }
+
+  removeItem(item: TodoItem): void {
+    this.#list.update(list => list.filter(i => i !== item));
+  }
+
+  clearCompletionStatus(): void {
+    this.#list.update(list => {
+      list.forEach(item => item.completed.set(false));
+      return [...list];
+    });
+  }
+}
