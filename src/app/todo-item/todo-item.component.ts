6606a4d274af4afeac24f347a41f4c5a36275dbf
Johnny Gérard <john@jgerard.dev>
2024-03-01 12:41:34 +0100

    feat: add TodoItemComponent

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoItem } from '../types/todo-item.class';
+import { TodoListService } from '../todo-list.service';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [
+    FormsModule,
+  ],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TodoItemComponent {
+  readonly todoItem = input.required<TodoItem>();
+
+  constructor(private _todoListService: TodoListService) { }
+
+  destroy(): void {
+    this._todoListService.removeItem(this.todoItem());
+  }
+}
