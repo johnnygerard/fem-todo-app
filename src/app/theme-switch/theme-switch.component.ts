1f8fae6060a20b6796c5f16b614d4b6a89f97607
Johnny Gérard <john@jgerard.dev>
2024-03-02 15:38:49 +0100

    refactor: use signal API for ThemeSwitchComponent

--- src/app/theme-switch/theme-switch.component.ts
+++ src/app/theme-switch/theme-switch.component.ts
@@ -1,54 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Theme } from './theme.enum';
+import { ChangeDetectionStrategy, Component, ModelSignal, effect, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-theme-switch',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './theme-switch.component.html',
   styleUrl: './theme-switch.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThemeSwitchComponent {
-  readonly #STORAGE_KEY = 'theme';
-  readonly #DARK_THEME_CLASS = 'dark-theme'; // Works like a boolean attribute (absent = light)
-  #_theme = this.#restoreTheme();
+  isDarkTheme: ModelSignal<boolean>;
+  readonly #STORAGE_KEY = 'isDarkTheme';
+  readonly #DARK_THEME_CLASS = 'dark-theme';
 
   constructor() {
-    // Set initial user theme (defaults to light theme)
-    if (this.#theme === Theme.DARK)
-      window.document.body.classList.add(this.#DARK_THEME_CLASS);
-  }
+    this.isDarkTheme = model(this.#userTheme);
 
-  toggleTheme(): void {
-    this.#theme = this.#theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-  }
+    effect(() => {
+      const isDarkTheme = this.isDarkTheme();
 
-  get #theme(): Theme {
-    return this.#_theme;
-  }
-
-  set #theme(theme: Theme) {
-    this.#_theme = theme;
-    localStorage.setItem(this.#STORAGE_KEY, theme);
-
-    // Add or remove dark theme class if theme is dark or light, respectively.
-    window.document.body.classList.toggle(this.#DARK_THEME_CLASS, theme === Theme.DARK);
+      window.document.body.classList.toggle(this.#DARK_THEME_CLASS, isDarkTheme);
+      localStorage.setItem(this.#STORAGE_KEY, isDarkTheme.toString());
+    });
   }
 
   /**
    * Restore user theme from local storage if present, otherwise use browser/system theme.
-   * @returns User's preferred theme
+   * @returns The user's preferred theme
    */
-  #restoreTheme(): Theme {
-    const theme = localStorage.getItem(this.#STORAGE_KEY);
+  get #userTheme(): boolean {
+    const isDarkTheme = localStorage.getItem(this.#STORAGE_KEY) as 'true' | 'false' | null;
 
-    if (theme)
-      return theme as Theme;
-    else {
-      // Note: matchMedia.matches returns false for unsupported media features
-      const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      return isDarkTheme ? Theme.DARK : Theme.LIGHT;
-    }
+    return isDarkTheme === null ?
+      window.matchMedia('(prefers-color-scheme: dark)').matches : // False if unsupported
+      isDarkTheme === 'true';
   }
 }
