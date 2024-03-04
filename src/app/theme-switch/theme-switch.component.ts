f6377de5c036903f5ec8652876a0728345243aa3
Johnny Gérard <john@jgerard.dev>
2024-03-04 11:41:50 +0100

    refactor: extract theme logic into theme service

--- src/app/theme-switch/theme-switch.component.ts
+++ src/app/theme-switch/theme-switch.component.ts
@@ -1,6 +1,7 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ModelSignal, effect, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-theme-switch',
@@ -14,30 +15,7 @@ import { FormsModule } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThemeSwitchComponent {
-  isDarkTheme: ModelSignal<boolean>;
-  readonly #STORAGE_KEY = 'isDarkTheme';
-  readonly #DARK_THEME_CLASS = 'dark-theme';
+  isDarkTheme = this._themeService.isDarkTheme;
 
-  constructor() {
-    this.isDarkTheme = model(this.#userTheme);
-
-    effect(() => {
-      const isDarkTheme = this.isDarkTheme();
-
-      window.document.body.classList.toggle(this.#DARK_THEME_CLASS, isDarkTheme);
-      localStorage.setItem(this.#STORAGE_KEY, isDarkTheme.toString());
-    });
-  }
-
-  /**
-   * Restore user theme from local storage if present, otherwise use browser/system theme.
-   * @returns The user's preferred theme
-   */
-  get #userTheme(): boolean {
-    const isDarkTheme = localStorage.getItem(this.#STORAGE_KEY) as 'true' | 'false' | null;
-
-    return isDarkTheme === null ?
-      window.matchMedia('(prefers-color-scheme: dark)').matches : // False if unsupported
-      isDarkTheme === 'true';
-  }
+  constructor(private _themeService: ThemeService) { }
 }
