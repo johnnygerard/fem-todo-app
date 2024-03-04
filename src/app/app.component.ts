325a5232cbd938fd3eb7a0e9415eae66cbc675cf
Johnny Gérard <john@jgerard.dev>
2024-03-04 12:36:21 +0100

    refactor: use NgOptimizedImage for theme background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
 import { TodoItemAdderComponent } from './todo-item-adder/todo-item-adder.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { ThemeBackgroundComponent } from './theme-background/theme-background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
+    ThemeBackgroundComponent,
     ThemeSwitchComponent,
     TodoItemAdderComponent,
     TodoListComponent,
